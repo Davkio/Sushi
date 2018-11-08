86c520c645fdc1e2d31ad933db4882cba9029fd5
Luke Haeffner <lhae0002@student.monash.edu>
2018-11-08 21:50:21 +1100

    testing client

--- sushi_client/src/index.js
+++ sushi_client/src/index.js
@@ -1,7 +1,75 @@
 "use strict";
 
 const Sushi = require("../../sushi_library/lib/index");
+const config = require('./config.json');
 
+class SushiBot {
+    constructor(options = {}) {
+        var client = new Sushi(options);
+        client.connect();
+
+        client.SushiEvent.on("READY", (e) => {
+            console.log("Logged in!");
+        });
+
+        client.SushiEvent.on("MESSAGE_CREATE", (e) => {
+            // only works for me right now
+            if (e.author.id == options.owner_id) {
+
+                // set the online presence
+                if (e.content.startsWith("sushi --setstatus")) {
+                    var args = e.content.split(" ");
+                    client.Bot.setStatus(args[2]);
+                }
+                if (e.content.startsWith("sushi --roles")) {
+                    client.Bot.getRoles(e).then((data) => {
+                        let role = data.find(data => data.name == 'Game night').id;
+                    });
+                }
+                if (e.content.startsWith("sushi --gameNight")) {
+                    let role = null;
+                    client.Bot.getRoles(e).then((data) => {
+                        role = data.find(data => data.name == 'Game night').id;
+                        //console.log(role);
+                    });
+
+                    client.Bot.sendMessage(e, "React to be involved with the next game night!\nRemove reaction to be removed").then((data) => {
+
+                        client.SushiEvent.on("MESSAGE_REACTION_ADD", (reac) => {
+                            if (reac.message_id == data.id && reac.channel_id == data.channel_id) {
+                                console.log(reac.guild_id + " " + reac.user_id + " " + role);
+                                client.Bot.userAddRole(reac.guild_id, reac.user_id, role).then(null, (err) => {
+                                    console.log("Error adding role");
+                                });
+                            }
+                        });
+                        client.SushiEvent.on("MESSAGE_REACTION_REMOVE", (reac) => {
+                            if (reac.message_id == data.id && reac.channel_id == data.channel_id) {
+                                console.log(reac.guild_id + " " + reac.user_id + " " + role);
+                                client.Bot.userRemoveRole(reac.guild_id, reac.user_id, role).then(null, (err) => {
+                                    console.log("Error removing role");
+                                });
+                            }
+                        });
+                    });
+                }
+            }
+        });
+
+
+    }
+
+    /**
+     * Take the event object, and return the message as an array
+     * @param {object} message 
+     */
+    getArguments(message) {
+
+    }
+}
+var client = new SushiBot(config);
+
+/*
 class SushiBot extends Sushi {
     constructor(options = {}) {
         super(options);
@@ -39,5 +107,6 @@ class SushiBot extends Sushi {
     }
 
 }
+*/
 
 module.exports = SushiBot;
